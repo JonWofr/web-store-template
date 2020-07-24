f93a533488555c74bf9a040d4a162da5a0a95d48
Jonas Wolfram <gartenspaten@web.de>
2020-07-24 19:42:47 +0200

    added routing and made some config tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { HomeComponent } from './core/views/home/home.component';
+import { DetailComponent } from './core/views/detail/detail.component';
+import { ContactComponent } from './core/views/contact/contact.component';
+import { AboutComponent } from './core/views/about/about.component';
+import { ProductsComponent } from './core/views/products/products.component';
+import { ShoppingCartComponent } from './core/views/shopping-cart/shopping-cart.component';
+import { ProfileComponent } from './core/views/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'products/:id',
+    component: DetailComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'shopping-cart',
+    component: ShoppingCartComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
